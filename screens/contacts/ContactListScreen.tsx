41419c863e36a254790b3d6d9b01246eac4aa5aa
Gabriel <gabriel.opereyra@gmail.com>
2020-06-17 23:57:44 -0300

    Apply react redux to listen changes

--- screens/contacts/ContactListScreen.tsx
+++ screens/contacts/ContactListScreen.tsx
@@ -1,54 +1,11 @@
-import React, { FunctionComponent } from 'react';
+import React from 'react';
 import { Text, SectionList, Button, View, StyleSheet } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
+import { connect, ConnectedProps } from 'react-redux';
 
 import Contact from '../../contacts';
 import ContactRow from './ContactRow';
 import { ContactNavigatorParams } from './ContactScreen';
-
-interface ContactsBySection {
-  [key: string]: Contact[];
-}
-
-type Props = {
-  contacts: Contact[];
-  navigation: StackNavigationProp<ContactNavigatorParams, 'ContactList'>;
-};
-
-const ContactListScreen: FunctionComponent<Props> = (props) => {
-  const contactsByRange = props.contacts.reduce((obj: ContactsBySection, contact: Contact) => {
-    const from = Math.floor(contact.id / 100) * 100;
-    const to = from + 99;
-    const section = `${from} - ${to}`;
-
-    return {
-      ...obj,
-      [section]: [...(obj[section] || []), contact],
-    };
-  }, {});
-
-  const contactsBySection = Object.keys(contactsByRange).map((range) => ({
-    title: range,
-    data: contactsByRange[range],
-  }));
-
-  const navigateContactDetail = (contact: Contact) => {
-    props.navigation.navigate('ContactDetail', { contact });
-  }; 
-
-  return (
-    <View style={styles.container}>
-      <Button title='Add contact' onPress={() => props.navigation.navigate('AddContact')} />
-      <SectionList
-        renderSectionHeader={(obj) => <Text>{obj.section.title}</Text>}
-        renderItem={(obj) => <ContactRow key={obj.item.id} contact={obj.item} onSelect={navigateContactDetail} />}
-        sections={contactsBySection}
-        style={{ flex: 1 }}
-        contentContainerStyle={{ paddingLeft: 50 }}
-      ></SectionList>
-    </View>
-  );
-};
 
 const styles = StyleSheet.create({
   container: {
@@ -57,4 +14,60 @@ const styles = StyleSheet.create({
   },
 });
 
-export default ContactListScreen;
+interface ContactsBySection {
+  [key: string]: Contact[];
+}
+
+type Props = {
+  navigation: StackNavigationProp<ContactNavigatorParams, 'ContactList'>;
+};
+
+interface RootState {
+  contacts: Contact[]
+}
+
+const mapState = (state: RootState) => ({
+  contacts: state.contacts
+});
+
+const connector = connect(mapState);
+type PropsFromRedux = ConnectedProps<typeof connector>;
+
+class ContactListScreen extends React.Component<Props & PropsFromRedux, {}> {
+  render() {
+    const contactsByRange = this.props.contacts.reduce((obj: ContactsBySection, contact: Contact) => {
+      const from = Math.floor(contact.id / 100) * 100;
+      const to = from + 99;
+      const section = `${from} - ${to}`;
+
+      return {
+        ...obj,
+        [section]: [...(obj[section] || []), contact],
+      };
+    }, {});
+
+    const contactsBySection = Object.keys(contactsByRange).map((range) => ({
+      title: range,
+      data: contactsByRange[range],
+    }));
+
+    const navigateContactDetail = (contact: Contact) => {
+      this.props.navigation.navigate('ContactDetail', { contact });
+    };
+
+    return (
+      <View style={styles.container}>
+        <Button title='Add contact' onPress={() => this.props.navigation.navigate('AddContact')} />
+        <SectionList
+          renderSectionHeader={(obj) => <Text>{obj.section.title}</Text>}
+          renderItem={(obj) => <ContactRow key={obj.item.id} contact={obj.item} onSelect={navigateContactDetail} />}
+          sections={contactsBySection}
+          style={{ flex: 1 }}
+          contentContainerStyle={{ paddingLeft: 50 }}
+        ></SectionList>
+      </View>
+    );
+  }
+};
+
+export default connector(ContactListScreen);
--- screens/contacts/ContactScreen.tsx
+++ screens/contacts/ContactScreen.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
+import React, { FunctionComponent } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import AddContactScreen from './AddContactScreen';
 import ContactListScreen from './ContactListScreen';
 import ContactDetailScreen from './ContactDetailScreen';
 import Contact from '../../contacts';
-import { store } from '../../state/store';
-import { Unsubscribe } from 'redux';
 
 export type ContactNavigatorParams = {
   ContactList: undefined;
@@ -15,40 +13,16 @@ export type ContactNavigatorParams = {
 
 const Stack = createStackNavigator<ContactNavigatorParams>();
 
-export default class ContactScreen extends React.Component<{}, { contacts: Contact[] }> {
-  private unsubscribe: Unsubscribe | undefined;
+const ContactScreen: FunctionComponent = () => (
+  <Stack.Navigator initialRouteName='ContactList'>
+    <Stack.Screen name='ContactList' component={ContactListScreen} options={{ headerTitle: 'Contacts' }} />
+    <Stack.Screen name='AddContact' component={AddContactScreen} options={{ headerTitle: 'New Contact' }} />
+    <Stack.Screen
+      name='ContactDetail'
+      component={ContactDetailScreen}
+      options={{ headerTitle: 'Contact Detail' }}
+    />
+  </Stack.Navigator>
+);
 
-  state = {
-    contacts: store.getState().contacts,
-  };
-
-  componentDidMount() {
-    this.unsubscribe = store.subscribe(() => {
-      this.setState({
-        contacts: store.getState().contacts
-      });
-    });
-  }
-
-  componentWillUnmount() {
-    if (this.unsubscribe) {
-      this.unsubscribe();
-    }
-  }
-
-  render() {
-    return (
-      <Stack.Navigator initialRouteName='ContactList'>
-        <Stack.Screen name='ContactList' options={{ headerTitle: 'Contacts' }}>
-          {(props) => <ContactListScreen {...props} contacts={this.state.contacts} />}
-        </Stack.Screen>
-        <Stack.Screen name='AddContact' component={AddContactScreen} options={{ headerTitle: 'New Contact' }} />
-        <Stack.Screen
-          name='ContactDetail'
-          component={ContactDetailScreen}
-          options={{ headerTitle: 'Contact Detail' }}
-        />
-      </Stack.Navigator>
-    );
-  }
-}
+export default ContactScreen;
